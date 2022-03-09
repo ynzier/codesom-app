e65c91998107680b5f85366d22494bc31d2a4359
Ynzier <kkunut.a@hotmail.com>
2022-03-10 04:55:17 +0700

    clean

--- http-common.tsx
+++
@@ -1,9 +0,0 @@
-import * as axios from "axios";
-
-export default axios.default.create({
-  // local 192.168.1.34:4000
-  // online api.knt-dev.online
-  baseURL: "http://localhost:4000/",
-  timeout: 3000,
-  timeoutErrorMessage: "การเชื่อมต่อขัดข้อง",
-});
