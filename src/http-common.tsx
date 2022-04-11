cffeaa7e52ce4a2861f23723cc31c46a8c7eb583
Ynzier <kkunut.a@hotmail.com>
2022-04-11 20:21:06 +0700

    .gitignore is now working

--- src/http-common.tsx
+++
@@ -1,9 +0,0 @@
-import * as axios from "axios";
-
-export default axios.default.create({
-  // local http://192.168.2.48:4000
-  // online https://api.knt-dev.online
-  baseURL: "https://api.knt-dev.online/",
-  timeout: 3000,
-  timeoutErrorMessage: "การเชื่อมต่อขัดข้อง",
-});
